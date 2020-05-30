461eba671ae19a8283fd298161b315832eb4fc60
Roberto Infantes <rob.car.inf@gmail.com>
2020-05-29 21:28:54 -0500

    added __main__ functionality in testing

--- test/test_api.py
+++ test/test_api.py
@@ -66,3 +66,6 @@ class APITestCase(unittest.TestCase):
         self.assertEqual(api.calculate_gallons_required(ft_room1), 6, "total gallons for room-1 (20,35,20) should be 6")
         self.assertEqual(api.calculate_gallons_required(ft_room2), 1, "total gallons for room-2 (20,20,20) should be 1")
         self.assertEqual(api.calculate_gallons_required(ft_room3), 14, "total gallons for room-3 (25,35,45) should be 14")
+
+if __name__ == '__main__':
+    unittest.main()
